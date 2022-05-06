a2e50028757c821cd9c39df5600f347e97b476ee
Kim-SeongSu <104110605+Kim-SeongSu@users.noreply.github.com>
2022-05-06 14:04:08 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # first-repo
+
+(Abstract) This repo is to learn how git works.
+
+## installation
+
+## how to start
+
+'''shell
+$ python3 hello.py
+'''
+
+## contributhing
+
+## lisense
---
+++ hello.py
@@ -0,0 +1,12 @@
+print('hello 1')
+
+for i in range(1,21):
+  if i % 15 == 0:
+    print('yeardream')
+  elif i % 3 == 0:
+    print('year')
+  elif i % 5 == 0:
+    print('dream')
+  else:
+    print(i)
+
